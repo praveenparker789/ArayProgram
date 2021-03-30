8b4a47cd26049d5c555409af82de4ad9f3fa9d1e
unknown <praveenparker10@gmail.com>
2021-03-31 02:29:01 +0530

    Code Added

---
+++ src/org/details/Introduction.java
@@ -0,0 +1,39 @@
+package org.details;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		System.out.println(" Details about me");
+		System.out.println("--------------------");
+
+		byte age=20;
+		System.out.println("My Age is "+ age);
+		
+		short myID=3476;
+				System.out.println("Indentification NO "+myID);
+				
+				int acNO=638352452;
+						System.out.println("My Account NUmber is "+acNO);
+		
+						long phNo=9894829753l;
+						System.out.println("Mobile Number is "+phNo);
+						
+						float highSecAvg=78.3465778f;
+						System.out.println("My 12th mark average is "+highSecAvg);
+							
+		double deg=88.569858687;
+		System.out.println("My degree mark Average is "+deg);
+		
+		char hei='6';
+		System.out.println(hei+" Feet Height");
+		
+		String email="praveenparker10@gmail.com";
+		System.out.println("My Email ID is "+email);
+		
+		boolean gotDeg=true;
+		System.out.println("Degree Holder "+gotDeg);
+				
+		
+	}
+
+}
